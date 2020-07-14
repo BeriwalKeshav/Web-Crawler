e8a312dac82440bb2ba0201347ea4122225cc489
Keshav Beriwal <keshavberiwal@Keshavs-MacBook-Air.local>
2020-07-14 12:51:45 +0530

    Directory Details Added

--- peperfry/spiders/my_spider.py
+++ peperfry/spiders/my_spider.py
@@ -47,6 +47,22 @@ class PepperFrySpider(scrapy.Spider):
             
     def parse_item(self,response,**meta):
         item_title=response.css('h1[class=v-pro-ttl\ pf-medium-bold-text]::text').get()
+        item_label=response.css('div[class=v-prod-comp-dtls-listitem] span[class=v-prod-comp-dtls-listitem-label]::text').getall() 
+        item_value=response.css('div[class=v-prod-comp-dtls-listitem] span[class=v-prod-comp-dtls-listitem-value\ pf-text-grey]::text').getall()
+        a=response.css('div[class=v-offer-price-wrap\ pf-margin-bottom5\ vipPrice] span::text').getall() 
+        item_offer='Rs '+a[1]
+        item_price='Rs'+ a[2][1:-6]
+        temp={}
+        for ind,i in enumerate(item_label):
+            temp.update({i:item_value[ind]})
+        diction = {
+            'title':item_title,
+            #for(ind,i in enumerate(item_label):
+                #i:item_value[ind],
+            'offer-price': item_offer,
+            'original': item_price
+        }
+        
         image_url=response.css('ul[class=vipImage__thumb-slider\ horizontal]')
         image_url_list=image_url.css('li[class=vipImage__thumb-each\ noClickSlide] a::attr(data-img)').getall() 
         CATEGORY_NAME = response.meta['dir_name']
@@ -54,8 +70,13 @@ class PepperFrySpider(scrapy.Spider):
         if not os.path.exists(ITEM_DIR_URL):
             os.makedirs(ITEM_DIR_URL)
             
-        for i,img_url in enumerate(image_url_list):
-            r = requests.get(img_url)
-            with open(os.path.join(ITEM_DIR_URL,"image_{}.jpg".format(i)),'wb') as f:
-                f.write(r.content)
+        with open(os.path.join(ITEM_DIR_URL,'Deatils.json'),'w') as f:
+            json.dump(temp,f)
+            f.write("\n")
+            json.dump(diction,f)    
+                
+        #for i,img_url in enumerate(image_url_list):
+            #r = requests.get(img_url)
+            #with open(os.path.join(ITEM_DIR_URL,"image_{}.jpg".format(i)),'wb') as f:
+                #f.write(r.content)
             
